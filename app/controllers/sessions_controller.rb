9511d039dbc70e9c006d1610bbeb605ed1c05b87
enoqueJonas <enoque.macanda@gmail.com>
2023-06-09 10:12:34 +0200

    ADD sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(name: params[:name])
+        if user
+            session[:user_id] = user.id
+            render json: { message: "Logged In Successfully" }
+        else
+            render json: { message: "Invalid Username" }
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        render json: { message: "Logged Out Successfully" }
+    end
+end
