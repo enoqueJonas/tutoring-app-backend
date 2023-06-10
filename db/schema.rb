5a9bcbca692c2af9b016fd6a0354d3086f5d3b4c
anas599 <anas599@yahoo.com>
2023-06-11 00:51:04 +0300

    update schema to include image link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_172116) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_10_214925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_172116) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "reservations", force: :cascade do |t|
