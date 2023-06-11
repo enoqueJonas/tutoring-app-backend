e29d2d860b0c6d46a292b93fd669f40ef2f273dc
anas599 <anas599@yahoo.com>
2023-06-11 12:34:44 +0300

    add user to class_subjects and add links to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_220631) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_11_090917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_220631) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "tutorName"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_class_subjects_on_user_id"
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -42,8 +44,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_220631) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "facebook"
+    t.string "linkedin"
+    t.string "twitter"
   end
 
+  add_foreign_key "class_subjects", "users"
   add_foreign_key "reservations", "class_subjects"
   add_foreign_key "reservations", "users"
 end
