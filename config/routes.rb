730d0f3afb58141430cebe1f5050f8bf27bf0315
enoqueJonas <enoque.macanda@gmail.com>
2023-06-13 16:34:07 +0200

    ADD logged_in route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :class_subjects
   resources :users
   post '/login', to: 'sessions#create'
+  get '/logged_in', to: 'sessions#logged_in'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
