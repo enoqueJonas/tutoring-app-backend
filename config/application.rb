bd490eb90749f4cfe16cfe1879743a48ff4c9416
anas599 <anas599@yahoo.com>
2023-06-18 01:13:55 +0300

    revert changes back for application.rb

--- config/application.rb
+++ config/application.rb
@@ -7,17 +7,13 @@ module TutoringAppBackend
   class Application < Rails::Application
     config.load_defaults 7.0
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'https://tutoring-front-end.onrender.com'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      end
-    end
-
     config.api_only = true
 
     # Enable session and cookies middleware
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_tutoring_key'
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    config.action_dispatch.cookies_same_site_protection = :strict
   end
 end
