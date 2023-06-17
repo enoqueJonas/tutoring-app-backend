05095452835343381172854c9134b07869011464
anas599 <anas599@yahoo.com>
2023-06-17 13:34:14 +0300

    cors wild card

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://tutoring-front-end.onrender.com"
+    origins "*"
 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: false
   end
 end
