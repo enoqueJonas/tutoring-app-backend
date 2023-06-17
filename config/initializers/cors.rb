4f920bde97c994b086c8d1c11c707d7405c941d7
anas599 <anas599@yahoo.com>
2023-06-18 00:47:09 +0300

    update resource

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "https://tutoring-front-end.onrender.com"
 
-    resource "/users",
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
