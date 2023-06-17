b62bc56ad240580f1ed7bba1f779d6c8651cec44
anas599 <anas599@yahoo.com>
2023-06-18 00:36:40 +0300

    change resource to users

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "/users"
+    origins "https://tutoring-front-end.onrender.com"
 
-    resource "*",
+    resource "/users",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
