08de4006e1409cb96d96860e8a61f4df0e5272f7
enoqueJonas <enoque.macanda@gmail.com>
2023-06-17 15:14:07 +0200

    UPDATE origins to local repo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://tutoring-front-end.onrender.com"
+    origins "http://127.0.0.1:3001"
 
     resource "*",
       headers: :any,
