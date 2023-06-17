ba7f4f548d3ce523718d1e5eadff7d324b27252c
anas599 <anas599@yahoo.com>
2023-06-17 14:58:51 +0300

    cors config changes

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,12 @@ Bundler.require(*Rails.groups)
 module TutoringAppBackend
   class Application < Rails::Application
     config.load_defaults 7.0
-
+config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
     config.api_only = true
 
     # Enable session and cookies middleware
