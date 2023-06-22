9b9fb1e3c4ed6fb0b9ee48b155c5579f29d32e68
anas599 <anas599@yahoo.com>
2023-06-22 18:08:20 +0300

    add class_subject unit test

---
+++ spec/models/class_subject.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe ClassSubject, type: :model do
+  it "has many reservations" do
+    class_subject = ClassSubject.new
+    expect(class_subject.reservations).to be_empty
+
+    reservation = Reservation.new
+    class_subject.reservations << reservation
+
+    expect(class_subject.reservations).to include(reservation)
+  end
+
+  it "destroys associated reservations when destroyed" do
+    class_subject = ClassSubject.new
+    reservation = Reservation.new
+    class_subject.reservations << reservation
+
+  end
+end
