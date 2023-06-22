54f8220e177e15b46f9baaaf6812fc4e4745d694
anas599 <anas599@yahoo.com>
2023-06-23 00:52:56 +0300

    update cors to new url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://tutoring-front-end.onrender.com"
+    origins "https://tutoring-app-front-41ebfdf96016.herokuapp.com/"
 
     resource "*",
       headers: :any,
